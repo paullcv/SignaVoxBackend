c7cf641a6bcc72ef6fa69d48a6a596bb26362d9d
paullcv <paul.cruz.4.pc@gmail.com>
2023-12-06 00:29:45 -0400

    Vista inicio retoque

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.windmill')
 @section('contenido')
-    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-        Inicio
-    </h2>
+    <div class="bg-white rounded p-4 mb-6 mt-2 text-center">
+        <h2 class="text-2xl font-semibold text-gray-900 dark:text-gray-200">
+            Inicio
+        </h2>
+    </div>
+
+    <div class="bg-white p-4 rounded-md shadow-md">
+        <p class="text-lg font-semibold">Has iniciado Sesión correctamente</p>
+    </div>
 @endsection
